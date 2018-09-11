3db788123947eece689973922f988527f4c6cc81
Tech-A23 <tech-a23@tech-a23s-air.lan>
2018-09-12 00:02:13 +0100

    2nd commit of calculator

---
+++ js/app.js
@@ -0,0 +1,263 @@
+console.log('working');
+
+// initial test
+
+
+// var x,y;
+// var a,b;
+// var c,d;
+// var e,f;
+//
+// var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?');
+// while (userinput !== "add" && userinput !== "subtract" && userinput !== "divide" && userinput !== "multiply") {
+//     userinput = prompt("Sorry invalid input, please enter either: add, subtract ,divide, or multiply");
+//
+//
+//
+//
+//
+//
+//
+// }
+// if (userinput === 'add'){
+//   x=prompt('type a number you want to add')
+//   y=prompt('type another number you want to add')
+//   var result= Number(x) + Number(y)
+//   alert('The answer is ' + result)
+//   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+// } else if (userinput === 'divide'){
+//   a=prompt('type a number you want to begin division')
+//   b=prompt('type another number you want to divide by')
+//   var result= Number(a) / Number(b)
+//   alert('The answer is ' + result)
+//   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+// } else if (userinput === 'multiply'){
+//   c=prompt('type a number you want to begin multiplication with')
+//   d=prompt('type another number you want to multiply by')
+//   var result= Number(c) * Number(d)
+//   alert('The answer is ' + result)
+//   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+// } else if (userinput === 'subtract'){
+//   e=prompt('type a number you want to begin subtraction with')
+//   f=prompt('type the second number you will subtract')
+//   var result= Number(e) - Number(f)
+//   alert('The answer is ' + result)
+//   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+// }
+//
+//
+//
+//
+//
+// var x,y;
+// var a,b;
+// var c,d;
+// var e,f;
+//
+// var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?');
+// while (userinput !== "add" && userinput !== "subtract" && userinput !== "divide" && userinput !== "multiply") {
+//     userinput = prompt("Sorry invalid input, please enter either: add, subtract ,divide, or multiply");
+//
+//
+//
+//
+//
+//
+//
+// }
+// if (userinput === 'add'){
+//   x=prompt('type a number you want to add')
+//   y=prompt('type another number you want to add')
+//   var result= Number(x) + Number(y)
+//   alert('The answer is ' + result)
+//   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+// } else if (userinput === 'divide'){
+//   a=prompt('type a number you want to begin division')
+//   b=prompt('type another number you want to divide by')
+//   var result= Number(a) / Number(b)
+//   alert('The answer is ' + result)
+//   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+// } else if (userinput === 'multiply'){
+//   c=prompt('type a number you want to begin multiplication with')
+//   d=prompt('type another number you want to multiply by')
+//   var result= Number(c) * Number(d)
+//   alert('The answer is ' + result)
+//   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+// } else if (userinput === 'subtract'){
+//   e=prompt('type a number you want to begin subtraction with')
+//   f=prompt('type the second number you will subtract')
+//   var result= Number(e) - Number(f)
+//   alert('The answer is ' + result)
+//   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+// }
+//
+// var x,y;
+// var a,b;
+// var c,d;
+// var e,f;
+//
+// var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?');
+// while (userinput !== "add" && userinput !== "subtract" && userinput !== "divide" && userinput !== "multiply") {
+//     userinput = prompt("Sorry invalid input, please enter either: add, subtract ,divide, or multiply");
+//
+//
+//
+//
+//
+//
+//
+// }
+// if (userinput === 'add'){
+//   x=prompt('type a number you want to add')
+//   y=prompt('type another number you want to add')
+//   var result= Number(x) + Number(y)
+//   alert('The answer is ' + result)
+//   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+// } else if (userinput === 'divide'){
+//   a=prompt('type a number you want to begin division')
+//   b=prompt('type another number you want to divide by')
+//   var result= Number(a) / Number(b)
+//   alert('The answer is ' + result)
+//   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+// } else if (userinput === 'multiply'){
+//   c=prompt('type a number you want to begin multiplication with')
+//   d=prompt('type another number you want to multiply by')
+//   var result= Number(c) * Number(d)
+//   alert('The answer is ' + result)
+//   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+// } else if (userinput === 'subtract'){
+//   e=prompt('type a number you want to begin subtraction with')
+//   f=prompt('type the second number you will subtract')
+//   var result= Number(e) - Number(f)
+//   alert('The answer is ' + result)
+//   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+// }
+//
+// var x,y;
+// var a,b;
+// var c,d;
+// var e,f;
+//
+// var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?');
+// while (userinput !== "add" && userinput !== "subtract" && userinput !== "divide" && userinput !== "multiply") {
+//     userinput = prompt("Sorry invalid input, please enter either: add, subtract ,divide, or multiply");
+//
+//
+//
+//
+//
+//
+//
+// }
+// if (userinput === 'add'){
+//   x=prompt('type a number you want to add')
+//   y=prompt('type another number you want to add')
+//   var result= Number(x) + Number(y)
+//   alert('The answer is ' + result)
+//   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+// } else if (userinput === 'divide'){
+//   a=prompt('type a number you want to begin division')
+//   b=prompt('type another number you want to divide by')
+//   var result= Number(a) / Number(b)
+//   alert('The answer is ' + result)
+//   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+// } else if (userinput === 'multiply'){
+//   c=prompt('type a number you want to begin multiplication with')
+//   d=prompt('type another number you want to multiply by')
+//   var result= Number(c) * Number(d)
+//   alert('The answer is ' + result)
+//   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+// } else if (userinput === 'subtract'){
+//   e=prompt('type a number you want to begin subtraction with')
+//   f=prompt('type the second number you will subtract')
+//   var result= Number(e) - Number(f)
+//   alert('The answer is ' + result)
+//   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+// }
+
+
+ //initial test//
+
+ // fully functioning calculator with minor bugs//
+
+ var x,y;
+ var a,b;
+ var c,d;
+ var e,f;
+ var keepcalculating = true;
+ var g;
+ var j,k;
+
+
+ while (keepcalculating) {
+     var userinput = prompt("Would you like to 'add', 'subtract','divide' or 'multiply' using the basic calculator/ or use the advanced calculator to 'squareroot' a single number or find the 'power' of two numbers/ or just 'quit' ?");
+     if (userinput === 'add'){
+       x=prompt('type a number you want to add')
+       y=prompt('type another number you want to add')
+       var result= Number(x) + Number(y)
+       alert('The answer is ' + result)
+       var userinput = prompt("Would you like to 'add', 'subtract','divide' or 'multiply' using the basic calculator/ or use the advanced calculator to 'squareroot' a single number or find the 'power' of two numbers/ or just 'quit' ?")
+     } else if (userinput === 'divide'){
+       a=prompt('type a number you want to begin division')
+       b=prompt('type another number you want to divide by')
+       var result= Number(a) / Number(b)
+       alert('The answer is ' + result)
+       var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator or just quit?')
+     } else if (userinput === 'multiply'){
+       c=prompt('type a number you want to begin multiplication with')
+       d=prompt('type another number you want to multiply by')
+       var result= Number(c) * Number(d)
+       alert('The answer is ' + result)
+       var userinput = prompt("Would you like to 'add', 'subtract','divide' or 'multiply' using the basic calculator/ or use the advanced calculator to 'squareroot' a single number or find the 'power' of two numbers/ or just 'quit' ?")
+     } else if (userinput === 'subtract'){
+       e=prompt('type a number you want to begin subtraction with')
+       f=prompt('type the second number you will subtract')
+       var result= Number(e) - Number(f)
+       alert('The answer is ' + result)
+       var userinput = prompt("Would you like to 'add', 'subtract','divide' or 'multiply' using the basic calculator/ or use the advanced calculator to 'squareroot' a single number or find the 'power' of two numbers/ or just 'quit' ?")
+     }else if (userinput === 'quit'){
+       break;
+     }else if (userinput === 'squareroot'){
+       g=prompt('type a number you want to squareroot')
+       var result= Math.sqrt(g)
+       alert('The answer is ' + result)
+     }else if (userinput === 'power'){
+       j=prompt('type the first number the power will be raised to')
+       k=prompt('type the second number')
+       var result=Math.pow(j, k)
+       alert('The answer is ' + result)
+     }
+
+
+
+
+
+
+
+
+ }
+ // if (userinput === 'add'){
+ //   x=prompt('type a number you want to add')
+ //   y=prompt('type another number you want to add')
+ //   var result= Number(x) + Number(y)
+ //   alert('The answer is ' + result)
+ //   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+ // } else if (userinput === 'divide'){
+ //   a=prompt('type a number you want to begin division')
+ //   b=prompt('type another number you want to divide by')
+ //   var result= Number(a) / Number(b)
+ //   alert('The answer is ' + result)
+ //   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+ // } else if (userinput === 'multiply'){
+ //   c=prompt('type a number you want to begin multiplication with')
+ //   d=prompt('type another number you want to multiply by')
+ //   var result= Number(c) * Number(d)
+ //   alert('The answer is ' + result)
+ //   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+ // } else if (userinput === 'subtract'){
+ //   e=prompt('type a number you want to begin subtraction with')
+ //   f=prompt('type the second number you will subtract')
+ //   var result= Number(e) - Number(f)
+ //   alert('The answer is ' + result)
+ //   var userinput = prompt('Would you like to add, subtract,divide or multiply using the basic calculator?')
+ // }
